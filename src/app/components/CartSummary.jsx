cea3ba91940c816228064670d852bc43180bc866
Ernesto Diaz <diaz37@usf.edu>
2025-11-03 00:10:50 -0500

    created CartSummary function

---
+++ src/app/components/CartSummary.jsx
@@ -0,0 +1,18 @@
+'use client';
+
+export default function CartSumamry({ cart, products, clearCart }) {
+    
+    const total = Object.entries(cart).reduce((sum, [id, qty]) => {
+        const product = products.find((p) => p.id === id);
+        return sum + (product.price * qty);  
+    }, 0);
+    
+    return (
+        <div>
+            <h2>Total: ${total}</h2>
+            <button onClick={() => clearCart({})}>
+                Clear Cart
+            </button>
+        </div>
+    )
+}
